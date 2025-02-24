fb1993939910229613c9f9b1bc39c97e7a878204
jhonrv1997 <138330958+jhonrv1997@users.noreply.github.com>
2025-02-24 16:23:00 -0500

    Add files via upload

---
+++ config/global.php
@@ -0,0 +1,8 @@
+<?php 
+define("DB_HOST","sql205.infinityfree.com");
+define("DB_NAME", "if0_36937719_perene");
+define("DB_USERNAME", "if0_36937719");
+define("DB_PASSWORD", "sXYXbKRfX4");
+define("DB_ENCODE","utf8");
+define("PRO_NOMBRE","if0_36937719_perene");
+?>
